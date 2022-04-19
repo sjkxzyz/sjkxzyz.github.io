f1e348a7bd3572491541aec3eddbb63270b9790d
zhaoyazheng0618 <1736684346@qq.com>
2022-04-19 15:38:20 +0800

    翰墨书院

---
+++ hmsy-catcher/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/res/scenes/2登录.fire",orientation:"portrait",jsList:[]};
