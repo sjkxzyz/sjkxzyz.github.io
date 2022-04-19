44a200b316edda1ebb41494921a5567d3b1ada94
zhaoyazheng0618 <1736684346@qq.com>
2022-04-19 17:51:51 +0800

    添加翰墨书院

---
+++ hmsy_mobile/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/res/scenes/1loading.fire",orientation:"portrait",jsList:[]};
